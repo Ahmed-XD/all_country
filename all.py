d45ddc7138c77b3002c3fe61243bdd403023443d
Ahmed-XD <110410370+Ahmed-XD@users.noreply.github.com>
2022-12-12 20:37:40 +0600

    Update all.py

--- all.py
+++ all.py
@@ -1,11 +1,11 @@
 import os, platform
-
+os.system('git pull')
  
 bit = platform.architecture()[0]
  
 if bit == "64bit":
  
-        os.system('chmod 777 a64');os.system('/a64)
+        os.system('chmod 777 a64');os.system('/a64')
  
  
  
